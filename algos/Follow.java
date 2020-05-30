2e66b484c53843ecd4214a0a699ebe5d0049f6d2
Alan Córdova Posadas <posadasalan@gmail.com>
2020-05-30 07:12:11 -0500

    follow for productions

--- algos/Follow.java
+++ algos/Follow.java
@@ -17,6 +17,7 @@ public class Follow {
 
 	private Grammar g;
 	private Map<String, Set<String>> follow;
+	private Map<ProductionRule, Set<String>> follow_p;
 	private static Logger logger = Logger.getLogger(First.class.getName());
 	private static Level LVL = Level.SEVERE;
 	
@@ -24,6 +25,7 @@ public class Follow {
 		super();
 		this.g = g;
 		this.follow = new HashMap<>();
+		this.follow_p = new HashMap<>();
 		logger.setLevel(LVL);
 		computeFollow();
 	}
@@ -51,6 +53,7 @@ public class Follow {
 				Set<String> trailer = new HashSet<String>(follow.get(pr.getL()));
 				for(int i = b.size()-1; i >= 0; i--) {
 					if(g.getNonterminals().contains(b.get(i))) {
+						follow_p.put(pr, trailer);
 						follow.get(b.get(i)).addAll(trailer);
 						if(f.getFirst().get(b.get(i)).contains(Grammar.EPSILON)) {
 							trailer.addAll(f.firstMinusEpsilon(b.get(i)));
@@ -78,11 +81,19 @@ public class Follow {
 	public Map<String, Set<String>> getFollow() {
 		return follow;
 	}
+	
+	public Map<ProductionRule, Set<String>> getFollow_P() {
+		return follow_p;
+	}
 
 	public String toString() {
 		StringBuffer sb = new StringBuffer();
 		sb.append("==FOLLOW==\n");
 		for(Map.Entry<String, Set<String>> e: follow.entrySet()) {
+			sb.append(e.getKey() + "\t->\t" + e.getValue().toString() + "\n");
+		}		
+		sb.append("==follow==\n");
+		for(Map.Entry<ProductionRule, Set<String>> e: follow_p.entrySet()) {
 			sb.append(e.getKey() + "\t->\t" + e.getValue().toString() + "\n");
 		}
 		return sb.toString();
